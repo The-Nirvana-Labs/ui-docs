175cb7c2af27c5c4a6273f93baa0a7683951396c
amandesai01 <amandesai01@gmail.com>
2024-02-07 22:22:47 +0530

    feat: Demo component

---
+++ components/Demonstration.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps<{
+  title?: string,
+}>()
+</script>
+
+<template>
+  <div class="flex-col w-full justify-center my-6 border p-3 rounded border-secondary">
+    <div class="text-lg font-bold my-3">{{ title }}</div>
+    <div role="tablist" class="tabs tabs-bordered">
+      <input type="radio" :name="title" role="tab" class="tab" aria-label="Preview" checked />
+      <div role="tabpanel" class="tab-content p-8">
+        <slot name="output" />
+      </div>
+
+      <input type="radio" :name="title" role="tab" class="tab" aria-label="Code" />
+      <div role="tabpanel" class="tab-content p-2">
+        <pre lang="html">
+          <slot name="code" />
+        </pre>
+      </div>
+    </div>
+  </div>
+</template>
