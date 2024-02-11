7e57c92c5e6a59f685ecb357fe206696d96277e9
Dhruv Parthasarathy <parthasarathy.d@northeastern.edu>
2024-02-11 09:48:30 -0800

    Abstracting the rendering and composing logic to separate components

---
+++ components/DemonstrationComposer.vue
@@ -0,0 +1,24 @@
+<template>
+  <DemonstrationsList :demonstrations="demonstrations" />
+</template>
+
+<script>
+export default {
+  name: "DemonstrationComposer",
+  props: {
+    demonstrations: {
+      type: Array,
+      required: true,
+    },
+  },
+  computed: {
+    formattedDemonstrations() {
+      return this.demonstrations.map((demo) => ({
+        ...demo,
+        // Perform necessary formatting
+        previewCode: JSON.stringify(demo.previewCode),
+      }));
+    },
+  },
+};
+</script>
---
+++ components/DemonstrationsList.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      <Demonstration
+        v-for="(demo, index) in demonstrations"
+        :key="index"
+        :title="demo.title"
+      >
+        <template #output>
+          <div v-html="demo.previewCode"></div>
+        </template>
+        <template #code>
+          {{ demo.previewCode }}
+        </template>
+      </Demonstration>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'DemonstrationsList',
+    props: {
+      demonstrations: {
+        type: Array,
+        required: true
+      }
+    }
+  }
+  </script>
+  
