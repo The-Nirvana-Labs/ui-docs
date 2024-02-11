a36b8eb7d561fcef09c374e5ddaacd9704cda597
Dhruv Parthasarathy <parthasarathy.d@northeastern.edu>
2024-02-11 09:48:50 -0800

    Exmple usage for abstraction

---
+++ pages/components/avatar.vue
@@ -0,0 +1,16 @@
+<template>
+  <DemonstrationComposer :demonstrations="avatarData" />
+</template>
+
+<script>
+import AvatarData from "../../data/avatarData.vue";
+
+export default {
+  name: "Avatar",
+  data() {
+    return {
+      avatarData: AvatarData.data // Assuming the data is exported under a `data` key
+    };
+  }
+};
+</script>
